9f33e6ab4f5c63f746b4feb820cb63c2058ac979
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-06 12:25:10 +0200

    first commit

---
+++ src/main/java/com/backend/dogwalks/DogwalksApplication.java
@@ -0,0 +1,13 @@
+package com.backend.dogwalks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogwalksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogwalksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/dogwalks/DogwalksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.dogwalks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogwalksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
