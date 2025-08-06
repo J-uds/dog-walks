e4c16af34425332307b059c8404a899605d01cf2
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-06 12:39:44 +0200

    build(user): create Role and CustomUser classes and add general atributes to them

---
+++ src/main/java/com/backend/dogwalks/user/CustomUser.java
@@ -0,0 +1,35 @@
+package com.backend.dogwalks.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 300)
+    private String password;
+
+    @Column(name = "img")
+    private String userImgUrl;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
+
