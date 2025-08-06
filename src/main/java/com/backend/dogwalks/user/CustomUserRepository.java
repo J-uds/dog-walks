9dd2679145d83dd6efaeee1fe97e5a6e09c441eb
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-06 15:52:40 +0200

    feature(CustomUserRepository): add @Repository and findUserByEmail method

--- src/main/java/com/backend/dogwalks/user/CustomUserRepository.java
+++ src/main/java/com/backend/dogwalks/user/CustomUserRepository.java
@@ -1,6 +1,11 @@
 package com.backend.dogwalks.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface CustomUserRepository extends JpaRepository <CustomUser, Long> {
+    Optional<CustomUser> findUserByEmail(String email);
 }
