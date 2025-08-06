7030d789b1b917f900db9af126bc510d71138572
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-06 16:19:20 +0200

    feat(CustomUserDetail): remove a space

--- src/main/java/com/backend/dogwalks/user/CustomUserDetail.java
+++ src/main/java/com/backend/dogwalks/user/CustomUserDetail.java
@@ -7,8 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-
-public class CustomUserDetail implements UserDetails {
+public class  CustomUserDetail implements UserDetails {
     private final CustomUser user;
 
     public CustomUserDetail(CustomUser user) {
