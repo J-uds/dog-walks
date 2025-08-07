6238c1d88dfda9a441d1e52f5057ef6b44adb042
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-07 16:34:09 +0200

    style(CustomUserDetail): add a comment to explain why there is email in getUsername method

--- src/main/java/com/backend/dogwalks/user/CustomUserDetail.java
+++ src/main/java/com/backend/dogwalks/user/CustomUserDetail.java
@@ -24,6 +24,7 @@ public class  CustomUserDetail implements UserDetails {
         return user.getPassword();
     }
 
+    //se registra con email
     @Override
     public String getUsername() {
         return user.getEmail();
