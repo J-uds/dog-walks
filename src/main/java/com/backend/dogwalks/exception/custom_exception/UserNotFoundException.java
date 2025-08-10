425faa612875c6f141c356627338f797c0840966
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-10 20:50:25 +0200

    refactor(Exceptions)/feat(GlobalExceptionHandler): change User for Entity / add entityNotFound and entityAlreadyExists exception handlers

--- src/main/java/com/backend/dogwalks/exception/custom_exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.backend.dogwalks.exception.custom_exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
