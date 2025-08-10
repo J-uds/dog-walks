36ae6e9d8e23bf1e42e072b0ec113d276a7d3275
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-10 18:49:09 +0200

    feat(JwtUtil): add methods to build, generate and isValid token, and extractAllClaims, extractUsername and getSignKey methods

--- src/main/java/com/backend/dogwalks/security/JwtUtil.java
+++ src/main/java/com/backend/dogwalks/security/JwtUtil.java
@@ -1,9 +1,66 @@
 package com.backend.dogwalks.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Slf4j
 @Component
 public class JwtUtil {
+
+    //inyecta valores desde application.properties
+    @Value("${app.jwt.secret}")
+    private String jwtSecretKey;
+
+    @Value("${app.jwt.expiration}")
+    private Long jwtExpiration;
+
+    public String generateToken(CustomUserDetail userDetail) {
+        return buildToken(userDetail, jwtExpiration);
+    }
+
+    private String buildToken(CustomUserDetail userDetail, long jwtExpiration) {
+        return Jwts
+                .builder()
+                .subject(userDetail.getUsername())
+                .claim("role", userDetail.getAuthorities().toString())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isValidToken(String token) {
+        try {
+            extractAllClaims(token);
+            return true;
+        } catch(Exception exception) {
+            return false;
+        }
+    }
+
+    private SecretKey getSignKey() {
+        byte[] bytes = Decoders.BASE64.decode(jwtSecretKey);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSignKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 }
