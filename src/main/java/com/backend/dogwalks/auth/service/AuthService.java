66d73c86b4de1a799cbe745cc57d9c8b828e9a5d
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-11 21:44:56 +0200

    build(AuthService): add class and registerUser method

---
+++ src/main/java/com/backend/dogwalks/auth/service/AuthService.java
@@ -0,0 +1,33 @@
+package com.backend.dogwalks.auth.service;
+
+import com.backend.dogwalks.auth.dto.UserRegisterMapper;
+import com.backend.dogwalks.auth.dto.UserRegisterRequest;
+import com.backend.dogwalks.auth.dto.UserRegisterResponse;
+import com.backend.dogwalks.exception.custom_exception.EntityAlreadyExistsException;
+import com.backend.dogwalks.user.entity.CustomUser;
+import com.backend.dogwalks.user.repository.CustomUserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final CustomUserRepository customUserRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(CustomUserRepository customUserRepository, PasswordEncoder passwordEncoder) {
+        this.customUserRepository = customUserRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserRegisterResponse registerUser(UserRegisterRequest request) {
+        if (customUserRepository.existsByEmail(request.email())) {
+            throw new EntityAlreadyExistsException("El e-mail " + request.email() + " ya está registrado");
+        }
+
+        CustomUser newUser = UserRegisterMapper.toEntity(request);
+        newUser.setPassword(passwordEncoder.encode(request.password()));
+        CustomUser saveUser = customUserRepository.save(newUser);
+
+        return UserRegisterMapper.toDto(saveUser);
+    }
+}
