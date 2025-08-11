5a7b76867e5fa58837680c6c7688209575efba88
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-11 12:56:26 +0200

    feat(SecurityConfig): start adding basic security by creating securityFilterChain method

--- src/main/java/com/backend/dogwalks/config/SecurityConfig.java
+++ src/main/java/com/backend/dogwalks/config/SecurityConfig.java
@@ -2,11 +2,25 @@ package com.backend.dogwalks.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, "/walks").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
