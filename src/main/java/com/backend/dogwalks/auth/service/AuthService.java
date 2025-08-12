8f6bdf66a471f773df56bd573e55eb02c8f2b13a
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-12 15:44:27 +0200

    feat(AuthService/Controller): add login methods

--- src/main/java/com/backend/dogwalks/auth/service/AuthService.java
+++ src/main/java/com/backend/dogwalks/auth/service/AuthService.java
@@ -1,12 +1,20 @@
 package com.backend.dogwalks.auth.service;
 
+import com.backend.dogwalks.auth.dto.login.LoginRequest;
+import com.backend.dogwalks.auth.dto.login.LoginResponse;
 import com.backend.dogwalks.auth.dto.register.RegisterMapper;
 import com.backend.dogwalks.auth.dto.register.RegisterRequest;
 import com.backend.dogwalks.auth.dto.register.RegisterResponse;
 import com.backend.dogwalks.exception.custom_exception.EntityAlreadyExistsException;
+import com.backend.dogwalks.security.CustomUserDetails;
+import com.backend.dogwalks.security.jwt.JwtUtil;
 import com.backend.dogwalks.user.entity.CustomUser;
 import com.backend.dogwalks.user.enums.Role;
 import com.backend.dogwalks.user.repository.CustomUserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +22,14 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final CustomUserRepository customUserRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
-    public AuthService(CustomUserRepository customUserRepository, PasswordEncoder passwordEncoder) {
+    public AuthService(CustomUserRepository customUserRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
         this.customUserRepository = customUserRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
     }
 
     public RegisterResponse registerUser(RegisterRequest request) {
@@ -31,5 +43,17 @@ public class AuthService {
         CustomUser saveUser = customUserRepository.save(newUser);
 
         return RegisterMapper.toDto(saveUser);
+    }
+
+    public LoginResponse loginUser(LoginRequest request) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
+
+        CustomUserDetails userDetails =  (CustomUserDetails) authentication.getPrincipal();
+
+        String token = jwtUtil.generateToken(userDetails);
+
+        CustomUser user = customUserRepository.findUserByEmail(userDetails.getUsername()).orElseThrow(() -> new EntityNotFoundException("Authenticated user with e-mail: " + userDetails.getUsername() + ", not found in data base"));
+
+       return new LoginResponse(token,"Bearer", user.getId(), user.getUsername(), user.getEmail(), user.getRole());
     }
 }
--- src/main/java/com/backend/dogwalks/config/SecurityConfig.java
+++ src/main/java/com/backend/dogwalks/config/SecurityConfig.java
@@ -1,17 +1,26 @@
 package com.backend.dogwalks.config;
 
+import com.backend.dogwalks.security.jwt.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,7 +33,8 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
-                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
@@ -33,5 +43,4 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
