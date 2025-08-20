02d701de02cadb305cac681969d8dc8125d3186b
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-20 19:24:03 +0200

    refactor(CustomUserService): remove unused imports

--- src/main/java/com/backend/dogwalks/user/service/CustomUserService.java
+++ src/main/java/com/backend/dogwalks/user/service/CustomUserService.java
@@ -1,12 +1,5 @@
 package com.backend.dogwalks.user.service;
 
-import com.backend.dogwalks.exception.custom_exception.UsernameNotFoundException;
-import com.backend.dogwalks.security.CustomUserDetails;
-import com.backend.dogwalks.user.repository.CustomUserRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
