a36dcc1002e932d915364e3758aa077cd70faa4c
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-24 15:14:38 +0200

    feat(AdminServiceTest): add setUp including walk and admin

--- src/test/java/com/backend/dogwalks/user/admin/AdminServiceTest.java
+++ src/test/java/com/backend/dogwalks/user/admin/AdminServiceTest.java
@@ -4,6 +4,7 @@ import com.backend.dogwalks.user.entity.CustomUser;
 import com.backend.dogwalks.user.enums.Role;
 import com.backend.dogwalks.user.repository.CustomUserRepository;
 import com.backend.dogwalks.user.service.AdminService;
+import com.backend.dogwalks.walk.entity.Walk;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -11,6 +12,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Admin Service Unit Tests")
@@ -23,9 +26,14 @@ public class AdminServiceTest {
     private AdminService adminService;
 
     private CustomUser testUser;
+    private CustomUser testAdmin;
+    private Walk testWalk;
     private static final Long USER_ID = 1L;
+    private static final Long ADMIN_ID = 2L;
     private static final String USERNAME = "Maria";
+    private static final String ADMIN_USERNAME = "Admin";
     private static final String EMAIL = "maria@test.com";
+    private static final String ADMIN_EMAIL = "admin@test.com";
     private static final String IMG_URL = "img.png";
 
     @BeforeEach
@@ -37,6 +45,27 @@ public class AdminServiceTest {
         testUser.setUserImgUrl(IMG_URL);
         testUser.setRole(Role.USER);
         testUser.setIsActive(true);
+        testUser.setWalks(new ArrayList<>());
+
+        testAdmin = new CustomUser();
+        testAdmin.setId(ADMIN_ID);
+        testAdmin.setUsername(ADMIN_USERNAME);
+        testAdmin.setEmail(ADMIN_EMAIL);
+        testAdmin.setUserImgUrl("admin.png");
+        testAdmin.setRole(Role.ADMIN);
+        testAdmin.setIsActive(true);
+        testAdmin.setWalks(new ArrayList<>());
+
+        testWalk = new Walk();
+        testWalk.setId(1L);
+        testWalk.setTitle("test walk");
+        testWalk.setDateTime(2025-08-24 09:30:00);
+        testWalk.setLocation("Narnia");
+        testWalk.setDuration(60);
+        testWalk.setDescription("bal bla bla");
+        testWalk.setWalkImgUrl("testWalk.png");
+        testWalk.setIsActive(true);
+        testWalk.setUser(testUser);
     }
 
     @Nested
