ca3ac7ee0cf3d127212c654a778e396fc22b7662
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-24 15:02:38 +0200

    feat(Walk): add more attributes and annotations to Walk

--- src/main/java/com/backend/dogwalks/walk/entity/Walk.java
+++ src/main/java/com/backend/dogwalks/walk/entity/Walk.java
@@ -1,10 +1,13 @@
 package com.backend.dogwalks.walk.entity;
 
 import com.backend.dogwalks.user.entity.CustomUser;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "walks")
@@ -20,12 +23,24 @@ public class Walk {
     @Column(nullable = false, length = 100)
     private String title;
 
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @Column(nullable = false, length = 100)
+    private String location;
+
+    private Integer duration;
+
+    @Column(columnDefinition = "TEXT")
     private String description;
 
     @Column(name = "img")
     private String walkImgUrl;
 
-    @ManyToOne
+    @Column(nullable = false, name = "is_active")
+    private Boolean isActive = true;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private CustomUser user;
 }
