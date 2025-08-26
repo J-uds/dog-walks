04e2838096a9263acf89c38617d23e52709e8954
J-uds <206530139+J-uds@users.noreply.github.com>
2025-08-26 21:21:07 +0200

    feat(WalkController): add create, update and delete methods

--- src/main/java/com/backend/dogwalks/walk/controller/WalkController.java
+++ src/main/java/com/backend/dogwalks/walk/controller/WalkController.java
@@ -1,13 +1,18 @@
 package com.backend.dogwalks.walk.controller;
 
+import com.backend.dogwalks.security.user.CustomUserDetails;
+import com.backend.dogwalks.user.entity.CustomUser;
 import com.backend.dogwalks.walk.dto.WalkDetailResponse;
+import com.backend.dogwalks.walk.dto.WalkRequest;
 import com.backend.dogwalks.walk.dto.WalkResponse;
 import com.backend.dogwalks.walk.dto.WalkSummaryResponse;
 import com.backend.dogwalks.walk.service.WalkService;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -62,4 +67,32 @@ public class WalkController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @PostMapping
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<WalkResponse> addWalk(@Valid @RequestBody WalkRequest request, Authentication authentication) {
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        CustomUser user = userDetails.getUser();
+        WalkResponse response = walkService.addWalk(request, user);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("@walkSecurity.canAccessWalk(#id, authentication.principal)")
+    public ResponseEntity<WalkResponse> updateWalk(@PathVariable Long id, @Valid @RequestBody WalkRequest request) {
+
+        WalkResponse response = walkService.updateWalk(id, request);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("@walkSecurity.canAccessWalk(#id, authentication.principal)")
+    public ResponseEntity<Void> deleteWalk(@PathVariable Long id) {
+
+        walkService.deleteWalk(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
