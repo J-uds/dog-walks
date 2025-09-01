2fc70bbf98c2426322f6531d33b7315a11ab9b1f
J-uds <206530139+J-uds@users.noreply.github.com>
2025-09-01 15:41:47 +0200

    feat(AdminControllerIntegrationTest): add test for getById, update and delete methods

--- src/test/java/com/backend/dogwalks/user/controller/AdminControllerIntegrationTest.java
+++ src/test/java/com/backend/dogwalks/user/controller/AdminControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.backend.dogwalks.user.controller;
 
 import com.backend.dogwalks.security.user.CustomUserDetails;
+import com.backend.dogwalks.user.dto.admin.AdminUserRequest;
 import com.backend.dogwalks.user.entity.CustomUser;
 import com.backend.dogwalks.user.enums.Role;
 import com.backend.dogwalks.user.repository.CustomUserRepository;
@@ -8,6 +9,7 @@ import com.backend.dogwalks.utils.IntegrationTestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -88,33 +90,197 @@ public class AdminControllerIntegrationTest {
         adminToken = integrationTestUtils.generateToken(admin);
     }
 
-    @Test
-    @DisplayName("GET /api/admin/users - must return paginated users when valid request")
-    void getAllUsersPaginated_shouldReturnPaginatedUsers_whenValidRequest() throws Exception{
+    @Nested
+    @DisplayName("GET Users")
+    class GetUsers {
 
-        mockMvc.perform(get("/api/admin/users")
-                .header("Authorization", "Bearer " + adminToken)
-                .param("page", "0")
-                .param("size", "10")
-                .param("sortBy", "id")
-                .param("sortDirection", "ASC")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content", hasSize(2)))
-                .andExpect(jsonPath("$.totalElements", is(2)))
-                .andExpect(jsonPath("$.Size", is(10)))
-                .andExpect(jsonPath("$.number", is(0)));
+        @Test
+        @DisplayName("GET /api/admin/users - must return paginated users when valid request")
+        void getAllUsersPaginated_shouldReturnPaginatedUsers_whenValidRequest() throws Exception{
+
+            mockMvc.perform(get("/api/admin/users")
+                    .header("Authorization", "Bearer " + adminToken)
+                    .param("page", "0")
+                    .param("size", "10")
+                    .param("sortBy", "id")
+                    .param("sortDirection", "ASC")
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.content", hasSize(2)))
+                    .andExpect(jsonPath("$.totalElements", is(2)))
+                    .andExpect(jsonPath("$.size", is(10)))
+                    .andExpect(jsonPath("$.number", is(0)));
+        }
+
+        @Test
+        @DisplayName("GET /api/admin/users - must return 400 bad request when size is zero or negative")
+        void getAllUsersPaginated_shouldReturnBadRequest_whenRequestSizeIsZero() throws Exception{
+
+            mockMvc.perform(get("/api/admin/users")
+                            .header("Authorization", "Bearer " + adminToken)
+                            .param("page", "0")
+                            .param("size", "0")
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        @DisplayName("GET /api/admin/users - must use safe defaults when invalid sort parameters")
+        void getAllUsersPaginated_shouldReturnSafeDefaults_whenInvalidSorts() throws Exception{
+
+            mockMvc.perform(get("/api/admin/users")
+                            .header("Authorization", "Bearer " + adminToken)
+                            .param("page", "0")
+                            .param("size", "10")
+                            .param("sortBy", "invalid")
+                            .param("sortDirection", "INVALID")
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.content", hasSize(2)))
+                    .andExpect(jsonPath("$.totalElements", is(2)))
+                    .andExpect(jsonPath("$.size", is(10)))
+                    .andExpect(jsonPath("$.number", is(0)));
+        }
+
+        @Test
+        @DisplayName("GET /api/admin/users - must limit max size when large size is requested")
+        void getAllUsersPaginated_shouldLimitMazSize_whenLargeSizeRequested() throws Exception{
+
+            mockMvc.perform(get("/api/admin/users")
+                            .header("Authorization", "Bearer " + adminToken)
+                            .param("page", "0")
+                            .param("size", "200")
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.size", lessThanOrEqualTo(100)))
+        }
+
+        @Test
+        @DisplayName("GET /api/admin/users/{id} - must return user when valid id")
+        void getUserById_shouldReturnUser_whenValidId() throws Exception{
+
+            mockMvc.perform(get("/api/admin/users({id}", user.getId())
+                            .header("Authorization", "Bearer " + adminToken)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.id", is(user.getId().intValue())))
+                    .andExpect(jsonPath("$.username", is("Maria")))
+                    .andExpect(jsonPath("$.email", is("maria@test.com")))
+                    .andExpect(jsonPath("$.role", is("USER")))
+                    .andExpect(jsonPath("$.isActive", is(true)));
+        }
+
+        @Test
+        @DisplayName("GET /api/admin/users/{id} - must return not found when invalid id")
+        void getUserById_shouldReturnNotFound_whenInvalidId() throws Exception{
+
+            mockMvc.perform(get("/api/admin/users({id}", 99L)
+                            .header("Authorization", "Bearer " + adminToken)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        @DisplayName("GET /api/admin/users - must return 401 unauthorized when regular user")
+        void getUserById_shouldReturnUnauthorized_whenRegularUser() throws Exception{
+
+            mockMvc.perform(get("/api/admin/users")
+                            .header("Authorization", "Bearer " + userToken)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isUnauthorized());
+        }
+
+        @Test
+        @DisplayName("GET /api/admin/users - must return 401 unauthorized when no token")
+        void getUserById_shouldReturnUnauthorized_whenNoToken() throws Exception{
+
+            mockMvc.perform(get("/api/admin/users")
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isUnauthorized());
+        }
     }
 
-    @Test
-    @DisplayName("GET /api/admin/users - must return 400 bad request when size is zero or negative")
-    void getAllUsersPaginated_shouldReturnBadRequest_whenRequestSizeIsZero() throws Exception{
+    @Nested
+    @DisplayName("Update Users")
+    class UpdateUsers {
 
-        mockMvc.perform(get("/api/admin/users")
-                        .header("Authorization", "Bearer " + adminToken)
-                        .param("page", "0")
-                        .param("size", "0")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
+        @Test
+        @DisplayName("PUT /api/admin/users/{id} - must update user when valid request")
+        void updateUser_shouldUpdateUser_whenValidRequest() throws Exception{
+
+            AdminUserRequest request = new AdminUserRequest(
+                    "Pepa",
+                    "pepa@test.com",
+                    "img.png",
+                    Role.USER,
+                    true
+            );
+
+            mockMvc.perform(put("/api/admin/users({id}", user.getId())
+                            .header("Authorization", "Bearer " + adminToken)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(integrationTestUtils.toJson(request)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.username", is("Pepa")))
+                    .andExpect(jsonPath("$.email", is("pepa@test.com")))
+                    .andExpect(jsonPath("$.role", is("USER")))
+                    .andExpect(jsonPath("$.isActive", is(true)));
+
+            CustomUser updatedUser = userRepository.findById(user.getId()).get();
+            assertEquals("Pepa", updatedUser.getUsername());
+            assertEquals("pepa@test.com", updatedUser.getEmail());
+        }
+
+        @Test
+        @DisplayName("PUT /api/admin/users/{id} - must return 409 conflict when emil already exist")
+        void updateUser_shouldReturnConflict_whenEmailExist() throws Exception{
+
+            AdminUserRequest request = new AdminUserRequest(
+                    "Pepa",
+                    "admin@test.com",
+                    "img.png",
+                    Role.USER,
+                    true
+            );
+
+            mockMvc.perform(put("/api/admin/users({id}", user.getId())
+                            .header("Authorization", "Bearer " + adminToken)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(integrationTestUtils.toJson(request)))
+                    .andExpect(status().isConflict());
+        }
     }
+
+    @Nested
+    @DisplayName("Delete Users")
+    class DeleteUsers {
+
+        @Test
+        @DisplayName("DELETE /api/admin/users/{id} - must delete user when valid request")
+        void deleteUser_shouldUpdateUser_whenValidRequest() throws Exception{
+
+            mockMvc.perform(delete("/api/admin/users({id}", user.getId())
+                            .header("Authorization", "Bearer " + adminToken)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isNoContent());
+
+            mockMvc.perform(delete("/api/admin/users({id}", user.getId())
+                            .header("Authorization", "Bearer " + adminToken)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        @DisplayName("DELETE /api/admin/users/{id} - must return 404 not found when invalid id")
+        void deleteUser_shouldReturnNotFound_whenInvalidId() throws Exception{
+
+            mockMvc.perform(delete("/api/admin/users({id}", 99L)
+                            .header("Authorization", "Bearer " + adminToken)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isNotFound());
+        }
+
+
+    }
+
 }
